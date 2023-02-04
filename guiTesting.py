4aaf6a8b208e3ef3d16fd80a267db3f21f5b047e
Riley Elwood <riley.elwood@uga.edu>
2023-02-03 21:52:46 -0500

    gui work in progress
    search bars made for the gui and some more work done

--- guiTesting.py
+++ guiTesting.py
@@ -4,26 +4,39 @@ from tkinter import *
 
 #main window
 root = Tk()
-root.geometry("200x150")
+root.geometry("1000x1000")
 frame = Frame(root)
 
 frame.pack()
 
-#left frame
-leftframe = Frame(root)
-leftframe.pack(side=LEFT)
-
-#right frame
-rightframe = Frame(root)
-rightframe.pack(side=RIGHT)
-
 #Label
-label = Label(frame, text="This is a label")
+label = Label(frame, text="Choose Your Adventure!",font = ('Comic Sans MS', 40))
 label.pack()
 
-#Button
-button1 = Button(leftframe, text="Button 1")
+#Read Me
+button1 = Button(frame, text="Button 1")
+button1.place(x=0, y=0)
 button1.pack(padx = 3, pady = 3)
+
+#Search Bar
+location_entry = Entry(frame, width=50)
+location_entry.insert(0,'City, State')
+location_entry.pack(padx = 3, pady = 3)
+
+date_entry = Entry(frame, width=50)
+date_entry.insert(0,'Date')
+date_entry.pack(padx = 3, pady = 3)
+
+people_entry = Entry(frame, width=50)
+people_entry.insert(0,'Number of People')
+people_entry.pack(padx = 3, pady = 3)
+
+#Search Button
+button2 = Button(frame, text="Search")
+button2.place(x=500, y=500)
+button2.pack(padx = 3, pady = 3)
+
+
 
 
 root.title("Test")
