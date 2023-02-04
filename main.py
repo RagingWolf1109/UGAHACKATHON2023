da988c8c70c13e106b207b6746d9a23945648f72
Riley Elwood <riley.elwood@uga.edu>
2023-02-03 20:28:26 -0500

    Update main.py
    test ui

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 from tkinter import *
+
+root = Tk()
+root.geometry("200x150")
+frame = Frame(root)
+
+frame.pack()
+
+leftframe = Frame(root)
+leftframe.pack(side=LEFT)
+
+rightframe = Frame(root)
+rightframe.pack(side=RIGHT)
+
+label = Label(frame, text="This is a label")
+label.pack()
+
+button1 = Button(leftframe, text="Button 1")
+button1.pack(padx = 3, pady = 3)
+
+
+root.title("Test")
+root.mainloop()
+
