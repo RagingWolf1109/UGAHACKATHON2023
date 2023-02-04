990c8b0664c23db593af0208ed710f9e6da3b20a
Cbghattas11 <104444572+Cbghattas11@users.noreply.github.com>
2023-02-03 21:15:05 -0500

    update Chris Ghattas

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from tkinter import *
 
 root = Tk()
-root.geometry("200x150")
+root.geometry("2000x1500")
+root.config(bg="#87CEEB")
+root.title("Search")
 frame = Frame(root)
+frame.pack()
+
+def search():
+    query = search_entry.get()
+    print("Searching for:", query)
 
 frame.pack()
 
@@ -15,10 +22,19 @@ rightframe.pack(side=RIGHT)
 label = Label(frame, text="This is a label")
 label.pack()
 
+
+def show_website():
+    print("Displaying website")
+
+button = Button(frame, text="Show Website", command=show_website)
+button.pack(side=LEFT)
 button1 = Button(leftframe, text="Button 1")
 button1.pack(padx = 3, pady = 3)
 
+search_entry = Entry(frame)
+search_entry.pack(side=LEFT)
 
-root.title("Test")
+search_button = Button(frame, text="Search", command=search)
+search_button.pack(side=LEFT)
+
 root.mainloop()
-
